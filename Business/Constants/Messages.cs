aac754565ca2c0c9b01d36208761c9f11bb30171
NurgulFirat <nurgulfirat02@gmail.com>
2022-02-25 23:33:34 +0300

    .netCore
    Engin Demiroğ kampı

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductsListed = "Ürünler listelendi";
+        public static string CategoryListed = "Kategoriler listelendi";
+        public static string ProductOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
+        public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var";
+        public static string CategoryLimitExceded="Kategori limit aşıldığı için ürün eklenemiyor";
+        public static string AuthorizationDenied = "Yetkiniz yok";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string UserRegistered = "Kayıt oldu";
+        public static string PasswordError = "Şifre hatalı";
+        public static string SuccessfulLogin = "Giriş başarılı";
+        public static string UserAlreadyExists = "Kulanıcı mevcut";
+        public static string AccessTokenCreated = "Token oluşturuldu";
+    }
+}
---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    //generic constraint(generic kısıt)
+    //class: referans tip
+    //IEntity: IEntity olabilir veya IEntity implemente eden bir nesne olabilir
+    //new(): new'lenebilir olmalı
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null); //filtre vermek zorunlu değil. 
+        T Get(Expression<Func<T, bool>> filter); //filtre vermek zorunlu
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        List<T> GetAllByCategoryId(int categoryId);
+    }
+}
